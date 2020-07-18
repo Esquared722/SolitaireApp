5e152f59c09da1857af027b05c8cd1e5a3554475
Eric <esquared722@gmail.com>
2020-07-17 22:44:03 -0400

    Drafted layout usin placeholders for where objects will go

--- App.js
+++ App.js
@@ -1,21 +1,48 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Solitaire App</Text>
-      <StatusBar style="auto" />
+      <View style={styles.top}>
+        <View style={styles.foundation}>
+          <Text style={{color: 'white'}}>Foundation</Text>
+        </View>
+        <View style={styles.deck}>
+          <Text style={{color: 'white'}}>Deck/Waste</Text>
+        </View>
+      </View>
+      <View style={styles.bottom}>
+        <Text style={{color: 'white'}}>Tableau</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flex: 1
   },
+  top: {
+    flex: 2,
+    flexDirection: 'row'
+  },
+  bottom: {
+    flex: 8,
+    backgroundColor: 'blue',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  foundation: {
+    flex: 6,
+    backgroundColor: 'green',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  deck: {
+    flex: 4,
+    backgroundColor: 'gray',
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
 });
